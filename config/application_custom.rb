6d8d1d4e4aa6a752fcfb914a5a0a23938b9ce35a
Ignacio Aliende García <ialiendeg@gmail.com>
2018-03-21 14:36:44 +0000

    Moves locale config to custom and adds timezone one

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :es
+    config.i18n.fallbacks = {'en' => 'es'}
+    config.time_zone = 'Atlantic/Canary'
   end
 end
